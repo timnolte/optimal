22ef40d3b15f31f8eadbac5db0dda16876d60c1b
Tim Nolte <tim.nolte@ndigitals.com>
2018-03-08 00:09:28 -0500

    Updates Plugin Header to Follow Current Standard
    Formats plugin comment header to more closely conform to current WordPress plugin standards.

--- optimal.php
+++ optimal.php
@@ -1,33 +1,33 @@
 <?php
-/*
-Name: Optimal OPML Browser
-Homepage: http://www.yabfog.com/wp/optimal/
-Description: Renders valid OPML from any source in a tree-like view. Links to external OPML files as well as RSS, RDF, and Atom feeds are expanded in place.
-Version: 0.4c (beta)
-Author: Dan MacTough
-Author URI: http://www.yabfog.com/
-GitHub Plugin URI: https://github.com/timnolte/optimal
-License: GPL
-
-Copyright (C) 2006 Dan MacTough
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-This is beta software, so please report any problems to
-danmactough AT yahoo DOT com
-*/
+/**
+ * Plugin Name: Optimal OPML Browser
+ * Plugin URI: http://www.yabfog.com/wp/optimal/
+ * Description: Renders valid OPML from any source in a tree-like view. Links to external OPML files as well as RSS, RDF, and Atom feeds are expanded in place.
+ * Author: Dan MacTough
+ * Author URI: http://www.yabfog.com/
+ * Version: 0.4c (beta)
+ * GitHub Plugin URI: https://github.com/timnolte/optimal
+ * License: GPL
+ * 
+ * Copyright (C) 2006 Dan MacTough
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * This is beta software, so please report any problems to
+ * danmactough AT yahoo DOT com
+ */
 
 //
 // Instantiate the class
